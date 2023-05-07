5759568f211f7bee61151329659415041a24d4c9
Siddharth Kshirsagar <kshirsagarsiddharth45@gmail.com>
2023-05-07 17:18:20 +0530

    added a get request

--- api/app.py
+++ api/app.py
@@ -4,23 +4,26 @@ from utilities import predict_pipeline
 app = Flask(__name__)
 
 
-@app.route("/predict", methods=["POST"])
+@app.route("/predict", methods=["POST","GET"])
 def predict():
-    data_json = request.json
-    try:
-        sample = data_json["text"]
-    except KeyError:
-        return jsonify({"error": "No text sent"})
-    
-    sample = [sample] 
-    predictions = predict_pipeline(sample) 
+    if request.method == "POST":
+        data_json = request.json
+        try:
+            sample = data_json["text"]
+        except KeyError:
+            return jsonify({"error": "No text sent"})
+        
+        sample = [sample] 
+        predictions = predict_pipeline(sample) 
 
-    try: 
-        result = jsonify(predictions) 
-    except TypeError as e: 
-        result = jsonify({'error': str(e)})
-    
-    return result 
+        try: 
+            result = jsonify(predictions) 
+        except TypeError as e: 
+            result = jsonify({'error': str(e)})
+        
+        return result 
+    else: 
+        return "Welcome"
 
 if __name__ == "__main__": 
     app.run(host = "0.0.0.0", debug=True)
